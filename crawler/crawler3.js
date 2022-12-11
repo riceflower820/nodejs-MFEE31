e8c9ba12ffc51712708735eda4a3f157d882bc44
riceflower820 <117623932+riceflower820@users.noreply.github.com>
2022-12-11 16:48:11 +0800

    edit

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -6,6 +6,7 @@
 // 3. 去讀官方文件
 const axios = require('axios');
 const fs = require('fs/promises');
+const moment = require('moment/moment');
 // http://54.71.133.152:3000/stocks?stockNo=2618&date=202211
 //2618, 2330, 2412
 
@@ -16,7 +17,8 @@ const fs = require('fs/promises');
     let number = await fs.readFile('stock.txt', 'utf-8');
     //console.log('await 來的', number);
     let stockNo = number;
-    let date = '20221111';
+    let date = moment().format('YYYYMMDD');
+    console.log(date);
     let response = await axios.get(`http://54.71.133.152:3000/stocks`, {
       params: {
         stockNo,
