51ca3fbf4ce281f8cff56a5128a754851ea02a30
riceflower820 <117623932+riceflower820@users.noreply.github.com>
2022-12-17 12:00:03 +0800

    edit test.js

--- crawler/test.js
+++ crawler/test.js
@@ -3,11 +3,11 @@ const mysql2 = require('mysql2/promise');
 
 (async()=>{
     const connection = await mysql2.createConnection({
-        host: '127.0.0.1',
-        port: 3306,
-        user: 'admin',
-        password: '',
-        database: 'stock_mfee31',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PWD,
+        database: process.env.DB_NAME,
     });
     let [data, fields] = await connection.query('SELECT * FROM `stocks`');
     // let data = result[0];
