4da72caae6277be7ee30264f590d48b45f97cc90
riceflower820 <117623932+riceflower820@users.noreply.github.com>
2022-12-29 19:21:37 +0800

    add insert

--- stock-be/server.js
+++ stock-be/server.js
@@ -14,7 +14,8 @@ let pool = mysql2.createPool({
     // 限制 pool 連線數的上限
     connectionLimit: 10,
   });
-
+  
+app.use(express.json());
 // 允許跨源存取
 // 預設是全部開放
 // 也可以做部分限制，參考 npm cors 的文件
@@ -33,76 +34,82 @@ let pool = mysql2.createPool({
 // localhost:3001/
 // app.use(express.static('./static'))
 // localhost:3001/2048/
-app.use('/2048', express.static('./static'));
+app.use('/2048',express.static('./static'));
 
-//中間件
-app.use((req, res, next) => {
-    console.log('這裡是一個中間件A');
-    req.mfee31 = '水母班';
-    next(); //拿掉網頁會一直轉圈圈
+// 中間件
+app.use((req ,res, next) => {
+    console.log('這裡是第一個中間件A');
+    // 可以自行創立任意改變
+    req.mfee31='水母班';
+    next();
+    // res.send('這裡是 A 中間件');
 });
 
-app.use((req, res, next) => {
-    console.log('這裡是一個中間件B');
-    req.dt = new Date().toISOString();
+
+app.use((req ,res, next) => {
+    console.log('這裡是第一個中間件B');
+    req.dt =new Date().toISOString();
     next();
 });
-
 
 
 // app.[Method]
 // get, post, put, patch, delete, option, head
 //路由中間件
-app.get('/', (req, res, next) => {
-    console.log('這裡是首頁2', req.mfee31, req.dt);
-    res.send('Hello Express 9'); //拿掉網頁會一直轉圈圈
+app.get('/',(req,res, next)=>{
+  console.log('這是頁面2',req.mfee31,req.dt);
+  res.send('Hello Express 9 首頁喔~~');
 });
 
-app.get('/api', (req, res, next) => {
-
-    res.json({
-        "name" : "John",
-        "age": 18
-    })
+app.get('/api',(req, res, next) =>{
+  res.json({
+      name: 'john',
+      age: 18,
+  });  
 });
 
+app.get('/api/stocks',async(req, res, next)=>{
+  // let results = await connection.query('SELECT * FROM stocks')
+  // let data =results[0];
+  console.log('這裡是/api/stocks')
+  let [data]=await pool.query('SELECT * FROM stocks')
+  res.json(data);
+});
 
-app.get('/api/stocks', async (req, res, next) => {
-    // let results = await connection.query('SELECT * FROM stocks');
-    // let data = results[0];
-  
-    let [data] = await pool.query('SELECT * FROM stocks');
-    res.json(data);
-  });
 
 
 app.get('/api/stocks/:stockId',async(req, res, next) => {
-    console.log('/api/stocks/:stockId=>',req.params.stockId);
-    let [data] = await pool.query('SELECT * FROM stock_prices WHERE stock_id=?',[req.params.stockId]);
-    res.json(data);
+  console.log('/api/stocks/:stockId=>',req.params.stockId);
+  let [data] = await pool.query('SELECT * FROM stock_prices WHERE stock_id=?',[req.params.stockId]);
+  res.json(data);
 });
 
-app.use((req, res, next) => {
-    console.log('這裡是的一個中間件 C');
-    next();
-  });
-  
-
-
-app.get('/test', (req, res, next) => {
-    console.log('這裡是 test 頁面');
-    res.send('Hello Test 1');
-});
-
-//放在所有的路由中間件的後面
-//前面所有的路由都比不到隊的網址時，就會掉到這裡來
-//--> 這裡是一個 404 的情況
-//利用了中間件會依照程式碼順序來執行的特性來達成
-app.use((req, res, next) => {
-    console.log('這裡是 404');
-    res.send('這裡沒有這個網頁啦')
+app.post('/api/stocks',async(req,res)=> {
+  console.log('POST /api/stocks',req.body);
+  let [data] = await pool.query('INSERT INTO stocks (id, name) VALUES (?,?)', [req.body.stockId, req.body.stockName]);
+  // console.log(results)
+  res.json(data);
 })
 
-app.listen(3001, () => {
+app.use((req, res, next) => {
+  console.log('這裡是的一個中間件 C');
+  next();
+});
+
+app.get('/test',(req,res)=>{
+  console.log('這是test頁面',req.dt);
+  res.send('Hello Test 1');
+});
+
+// 放在所有路由中間的後面
+// 前面所有的路由對比不到對的網址，就會掉到這裡來
+// --> 就是一個404的情況
+// 利用了中間件會依照比對程式碼順序來執行的特性來達成
+app.use((req, res, next) => {
+  console.log('這裡是404')
+  res.send('沒有這個網頁啦!!')
+})
+// (數字看自己設多少port,但不可與其他重複。ex:react=>3000、mySQL=>3006)
+app.listen(3001,()=>{
   console.log('Server running at port 3001');
 });
